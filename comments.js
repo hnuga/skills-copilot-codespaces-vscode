f1935db5f78401fffb85833b1c889224ac300b9b
hnuga <140320891+hnuga@users.noreply.github.com>
2023-07-29 14:05:45 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,63 +1,58 @@
 // Create web server
-// run: node comments.js
-// test: curl -i http://localhost:3000/comments
-// test: curl -i http://localhost:3000/comments/1
-// test: curl -i -X POST -H 'Content-Type: application/json' -d '{"body": "Body of comment", "postId": 1}' http://localhost:3000/comments
-// test: curl -i -X PUT -H 'Content-Type: application/json' -d '{"body": "Update body of comment"}' http://localhost:3000/comments/1
-// test: curl -i -X DELETE http://localhost:3000/comments/1
-// test: curl -i -X DELETE http://localhost:3000/comments/2
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var _ = require('lodash');
-var comments = [
-  {id: 1, body: "some comment", postId: 1},
-  {id: 2, body: "some comment", postId: 1}
-];
-var commentNextId = 3;
-
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/comments', function(req, res) {
-  res.json(comments);
+// Importing the model
+const Comment = require('./models/comments.js');
+
+// Create a comment
+app.post('/comments', (req, res) => {
+    Comment.create({
+        name: req.body.name,
+        comment: req.body.comment
+    }).then(comment => res.json(comment));
 });
 
-app.get('/comments/:id', function(req, res) {
-  var commentId = parseInt(req.params.id, 10);
-  var matchedComment = _.find(comments, {id: commentId});
-
-  if (matchedComment) {
-    res.json(matchedComment);
-  } else {
-    res.status(404).send();
-  }
+// Get all comments
+app.get('/comments', (req, res) => {
+    Comment.findAll().then(comments => res.json(comments));
 });
 
-app.post('/comments', function(req, res) {
-  var body = req.body;
-
-  if (!_.isString(body.body) || !_.isNumber(body.postId)) {
-    return res.status(400).send();
-  }
-
-  var comment = _.pick(body, 'body', 'postId');
-  comment.id = commentNextId++;
-
-  comments.push(comment);
-  res.json(comment);
+// Get comment by id
+app.get('/comments/:id', (req, res) => {
+    Comment.findAll({
+        where: {
+            id: req.params.id
+        }
+    }).then(comment => res.json(comment));
 });
 
-app.delete('/comments/:id', function(req, res) {
-  var commentId = parseInt(req.params.id, 10);
-  var matchedComment = _.find(comments, {id: commentId});
-
-  if (!matchedComment) {
-    res.status(404).send();
-  } else {
-    comments = _.without(comments, matchedComment);
-    res.json(matchedComment);
-  }
+// Update comment by id
+app.put('/comments/:id', (req, res) => {
+    Comment.update({
+        name: req.body.name,
+        comment: req.body.comment
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then(comment => res.json(comment));
 });
 
-app.put('/comments/:id', function(req 
+// Delete comment by id
+app.delete('/comments/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(comment => res.json(comment));
+});
+
+// Listen to port
+app.listen(port, () => console.log(`Server started on port ${port}`));
